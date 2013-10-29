c7937ff9671e9e0b26942b9ccf297e18838511fe
Jing Yang <ditsing@gmail.com>
2013-10-29 23:24:55 +1100

    Put vimfiles under vundle.

--- setup_vim.sh
+++ setup_vim.sh
@@ -6,21 +6,25 @@ mkdir -p ~/.vim/.swap
 mkdir -p ~/.vim/.undo
 mkdir -p ~/.vim/bundle
 
+cd ~/.vim/bundle
+
 echo "Downloading vimfiles of ditsing from Github"
-git clone https://github.com/ditsing/vimfiles.git ~/.vim/ditsing
-cp ~/.vim/ditsing/vimrc ~/.vimrc
+git clone https://github.com/ditsing/vimfiles.git
+ln -s vimfiles/vimrc ~/.vimrc
 
 echo "Installing plugins"
 git clone https://github.com/gmarik/vundle.git
-vim -u ~/.vim/ditsing/bundles.vim +BundleInstall +qall
+vim -u vimfiles/bundles.vim +BundleInstall +qall
 
 echo "Installing Airline fonts"
-git clone https://github.com/Lokaltog/powerline-fonts.git ~/.vim/powerline-fonts
+git clone https://github.com/Lokaltog/powerline-fonts.git
 mkdir -p ~/.fonts
 cp powerline-fonts/UbuntuMono/*.ttf ~/.fonts
 
 echo "Copy YCM general configuration files."
-cp ~/.vim/ditsing/general_ycm_extra_conf.py ~/.vim/.ycm_extra_conf.py
+cp vimfiles/general_ycm_extra_conf.py ~/.vim/.ycm_extra_conf.py
 echo "You need to compile YouCompleteMe. Install cmake first, then run"
 echo '	cd ~/.vim/bundle/YouCompleteMe'
 echo '	./install.sh --clang-completer'
+
+cd -
